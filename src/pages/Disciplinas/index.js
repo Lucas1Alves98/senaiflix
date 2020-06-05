e9d64d24d940cdcfb9503832b0773dc500d4f179
Lucas Alves <lucas1alves98@gmail.com>
2020-06-05 09:15:31 -0300

    atualizando pages

--- src/pages/Disciplinas/index.js
+++ src/pages/Disciplinas/index.js
@@ -1,24 +1,37 @@
+  
+import React from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import Style from "./style";
+import logoImg from "../../assets/logo.png";
 
-export default function Cursos({ navigation }) {
+export default function Disciplinas({ navigation }) {
   return (
     <View style={Style.container}>
       <View style={Style.header}>
-        <Image source={logoImg} />
+        <View style={Style.header}>
+          <Image source={logoImg} />
+        </View>        
       </View>
-
-      <Text style={Style.title}>Cursos</Text>
-
       <View style={Style.content}>
-        <Text style={Style.coursesName}>Desenvolvimento de Sistemas</Text>
+        <Text style={Style.cursoNome}>Desenvolvimento de Sistemas</Text>
         <TouchableOpacity
           style={Style.coursesSubjects}
-          onPress={() => navigation.navigate("Disciplinas")}
-        >
+          onPress={() => navigation.navigate("Logica")}>
           <Text style={Style.coursesSubjectsText}>
-            Ver disciplinas do curso
+            Lógica de programação
           </Text>
         </TouchableOpacity>
-      </View>
+
+        <TouchableOpacity
+          style={Style.coursesSubjects}
+          onPress={() => navigation.navigate("Desenvolvimento1")}>
+          <Text style={Style.coursesSubjectsText}>
+            Desenvolvimento de Sistemas 1
+          </Text>
+        </TouchableOpacity>
+
+      </View>      
     </View>
   );
 }
